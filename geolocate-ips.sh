2dcc926cd9ac2a27bba9efdd6ada0d67ce15877f
vitor-godoi <73084996+vitor-godoi@users.noreply.github.com>
2022-06-17 17:48:29 -0300

    Update geolocate-ips.sh

--- geolocate-ips.sh
+++ geolocate-ips.sh
@@ -6,7 +6,7 @@ read listaip
 for ip in `cat $listaip`;
 do
 
-location=`proxychains curl -L -k -s https://iplist.cc/api/$ip|grep countryname|cut -d':' -f2`
+location=`curl -L -k -s https://iplist.cc/api/$ip|grep countryname|cut -d':' -f2`
 echo $ip $location
 
 done
